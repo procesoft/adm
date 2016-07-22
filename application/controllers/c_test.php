26bb111b0b667ff031e8fbf82a3b8487069dbea8
Enrique305 <enriquegarcia@indatcom.mx>
2016-07-22 11:06:28 -0500

    Update c_test.php

--- application/controllers/c_test.php
+++ application/controllers/c_test.php
@@ -32,7 +32,7 @@ class C_test extends CI_Controller {
             if (empty($v_modulos_minimos)) {
                 $v_modulos_minimos = '';
             }
-            $query = $this->db->query('call sp_get_reporte_apis(?,?,?,?,?)',array('','',1,1,0));
+            $query = $this->db->query('call sp_get_reporte_apis(?,?,?,?,?)',array($v_fecha_inicio,$v_fecha_fin,$v_modulos_minimos,1,0));
            
             
             if (!$query) {
